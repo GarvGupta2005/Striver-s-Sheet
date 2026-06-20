f8b27079bba2108936106868acdecbaf6568a151
GarvGupta2005 <garvg310@gmail.com>
2026-06-20 19:02:14 +0530

    Code updated

---
+++ Stack & Queue/Balanced Parenthesis/code.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> S = new Stack<>();
+
+        for(int i = 0; i<s.length(); i++)
+        {
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '[' || ch == '{')
+            {
+                S.push(ch);
+            }
+            else
+            {
+                if(S.isEmpty())
+                {
+                    return false;
+                }
+                else
+                {
+                    char top = S.pop();
+
+                    if(ch == ')' && top != '(')
+                    {
+                        return false;
+                    }
+                    if(ch == ']' && top != '[')
+                    {
+                        return false;
+                    }
+                    if(ch == '}' && top != '{')
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
